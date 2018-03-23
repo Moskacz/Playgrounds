20147a3b3878202b6bdcfe3da15ea499bd73530e
Michał Moskała <moskala.michal@gmail.com>
2018-03-23 18:45:48 +0100

    Playground for GrafientView

--- Calendar.playground/Contents.swift
+++ Calendar.playground/Contents.swift
@@ -8,4 +8,78 @@ cal.component(.weekOfYear, from: Date())
 cal.component(.day, from: Date())
 cal.component(.month, from: Date())
 cal.component(.year, from: Date())
-let components = cal.dateComponents([.day, .weekOfYear, .month, .year], from: Date())
+cal.ordinality(of: .month, in: .day, for: Date())
+let components = cal.dateComponents([.day, .weekOfYear, .month, .year], from: Date().addingTimeInterval(1000000))
+let era = cal.dateComponents([.era], from: Date())
+let day = cal.ordinality(of: .day, in: .era, for: Date())!
+let weekDay = cal.component(.weekday, from: Date())
+let id: Calendar.Identifier = cal.identifier
+
+
+class MyClass: CustomStringConvertible {
+    var description: String {
+        return "test"
+    }
+}
+
+final class WeakBox<T:AnyObject>: CustomStringConvertible {
+    
+    public weak var boxed: T?
+    
+    public init(value: T) {
+        boxed = value
+    }
+    
+    var description: String {
+        return "test"
+    }
+    
+}
+
+class WeakArray<T:AnyObject>{
+    private let items: [WeakBox<T>]
+    
+    init(items: [T]) {
+        self.items = items.map { WeakBox(value: $0) }
+    }
+}
+
+extension WeakArray: Sequence {
+    typealias Iterator = WeakArrayIterator<T>
+    
+    func makeIterator() -> WeakArrayIterator<T> {
+        return WeakArrayIterator<T>(itemsIterator: items.makeIterator())
+    }
+}
+
+struct WeakArrayIterator<T:AnyObject>: IteratorProtocol {
+    
+    typealias Element = T
+    private var itemsIterator: IndexingIterator<[WeakBox<T>]>
+    
+    init(itemsIterator: IndexingIterator<[WeakBox<T>]>) {
+        self.itemsIterator = itemsIterator
+    }
+    
+    mutating func next() -> T? {
+        return itemsIterator.next()?.boxed
+        guard let nextItem = itemsIterator.next() else {
+            return nil
+        }
+        
+        
+        
+        if nextItem.boxed != nil {
+            return nextItem.boxed
+        } else {
+            return next()
+        }
+    }
+}
+
+let array = WeakArray(items: [MyClass(), MyClass(), MyClass()])
+for v in array {
+    print(v)
+}
+
+
---
+++ GradientView.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let view = UIView()
+view.backgroundColor = .blue
+view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+
+
+
+PlaygroundPage.current.liveView = view

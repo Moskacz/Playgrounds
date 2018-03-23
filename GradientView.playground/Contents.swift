943db920ba0a1ce6a6159b3a952b671f60d42b46
Michał Moskała <moskala.michal@gmail.com>
2018-03-23 18:58:08 +0100

    Setup gradient data

--- GradientView.playground/Contents.swift
+++ GradientView.playground/Contents.swift
@@ -3,22 +3,42 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+protocol GradientRepresenting {
+    var colors: [UIColor] { get }
+    var startPoint: CGPoint { get }
+    var endPoint: CGPoint { get }
+}
+
 class GradientView: UIView {
     
     override class var layerClass: AnyClass {
         return CAGradientLayer.self
     }
 
-    var gradient: CAGradientLayer {
+    private var gradientLayer: CAGradientLayer {
         return layer as! CAGradientLayer
     }
     
+    var gradient: GradientRepresenting? {
+        didSet {
+            gradientLayer.colors = gradient?.colors.map { $0.cgColor }
+            gradientLayer.startPoint = gradient?.startPoint ?? CGPoint.zero
+            gradientLayer.endPoint = gradient?.endPoint ?? CGPoint.zero
+        }
+    }
 }
 
 let gradientView = GradientView()
 gradientView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-gradientView.gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
-gradientView.gradient.startPoint = CGPoint(x: 0, y: 0)
-gradientView.gradient.endPoint = CGPoint(x: 1, y: 1)
+
+struct Gradient: GradientRepresenting {
+    var colors: [UIColor]
+    var startPoint: CGPoint
+    var endPoint: CGPoint
+}
+
+gradientView.gradient = Gradient(colors: [UIColor.orange, UIColor.yellow],
+                                 startPoint: CGPoint.zero,
+                                 endPoint: CGPoint(x: 1, y: 1))
 
 PlaygroundPage.current.liveView = gradientView

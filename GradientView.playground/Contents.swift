325911e17fbcc417fa1dbaf76f965be2520f7903
Michał Moskała <moskala.michal@gmail.com>
2018-03-23 18:51:04 +0100

    GradientView working

--- GradientView.playground/Contents.swift
+++ GradientView.playground/Contents.swift
@@ -3,10 +3,22 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let view = UIView()
-view.backgroundColor = .blue
-view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+class GradientView: UIView {
+    
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
 
+    var gradient: CAGradientLayer {
+        return layer as! CAGradientLayer
+    }
+    
+}
 
+let gradientView = GradientView()
+gradientView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+gradientView.gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+gradientView.gradient.startPoint = CGPoint(x: 0, y: 0)
+gradientView.gradient.endPoint = CGPoint(x: 1, y: 1)
 
-PlaygroundPage.current.liveView = view
+PlaygroundPage.current.liveView = gradientView

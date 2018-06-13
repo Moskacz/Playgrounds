f46eeac950300dec6aed86102c5f1a15793f593c
Michał Moskała <moskala.michal@gmail.com>
2018-06-13 22:36:40 +0200

    Tests for one-element range

--- Ranges.playground/Contents.swift
+++ Ranges.playground/Contents.swift
@@ -3,3 +3,11 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let oneElementRange = 2...2
+oneElementRange.contains(2)
+oneElementRange.lowerBound == oneElementRange.upperBound
+oneElementRange.count
+
+
+
